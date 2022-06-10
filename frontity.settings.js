284d371542a4ad09742ff420a9cecb14f72303f1
Patrick Tangney <ptangney@outlook.com>
2022-06-10 23:31:47 +0100

    updated

--- frontity.settings.js
+++ frontity.settings.js
@@ -15,7 +15,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://test.frontity.org",
+          "url": "https://blog.patangney.com/",
           "postTypes": [
             {
               type: "destinations",
